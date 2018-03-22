6d795014ac013a9511f2184c4acb10d4c700af1b
anthony.brito.silva.gabriel@gmail.com <girlene.andradenun@gmail.com>
2018-03-22 11:38:00 -0300

    Peojeto iniciado

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAD2D180222
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+          
+            Console.WriteLine("escreva 2 números");
+            int v = int.Parse(Console.ReadLine());
+            Console.WriteLine("digite outro número");
+           int b= int.Parse(Console.ReadLine());
+           
+            if ()
+            axaaaxaaaxaaxaaaxaxaxaxse xo sexo sexo sexo sexo sexo seo {  
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
